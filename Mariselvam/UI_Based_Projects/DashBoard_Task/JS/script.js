aad3bbfd9ade4a1f199b414bc2f166375350e8a9
Mariselvam-33 <217136mech@gmail.com>
2023-02-13 15:21:37 +0530

    Modified_Chart

---
+++ Mariselvam/UI_Based_Projects/DashBoard_Task/JS/script.js
@@ -0,0 +1,93 @@
+var chart_js = {
+	"legend": {
+		"layout": "vertical"
+	},
+	"chart": {
+		"plot": {
+			"plotoptions": {
+				"pie": {
+					"innerRadius": "65%",
+					"marker": {
+						"enabled": false
+					}
+				}
+			}
+		}
+	},
+	"metadata": {
+		"axes": {
+			"x": [
+				0
+			],
+			"y": [
+				[
+					1
+				]
+			],
+			"tooltip": [
+				0,
+				1
+			]
+		},
+		"columns": [
+			{
+				"dataindex": 0,
+				"columnname": "Year(Date)",
+				"datatype": "time",
+				"time": {
+					"subfunction": "YEAR"
+				}
+			},
+			{
+				"dataindex": 1,
+				"columnname": "Sum(Sales)",
+				"datatype": "numeric"
+			}
+		]
+	},
+	"seriesdata": {
+		"chartdata": [
+			{
+				"type": "pie",
+				"data": [
+					[
+						[
+							"2010",
+							225881.08
+						],
+						[
+							"2011",
+							297103.28
+						],
+						[
+							"2012",
+							338301.21
+						]
+					]
+				]
+			}
+		]
+	},
+	"canvas": {
+		"title": {
+			"show": false,
+			"text": "Simple donut"
+		},
+		"subtitle": {
+			"show": false
+		},
+		"border": {
+			"show": false
+		},
+		"shadow": {
+			"show": false
+		}
+	},
+	"map": {
+		"labels": {
+			"show": false
+		}
+	}
+}
+
+var Chart_Obj = new $ZC.charts(Session_Devices_chart_view,chart_js)
